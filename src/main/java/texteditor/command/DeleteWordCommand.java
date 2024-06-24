76e573624ea8d0155b200998221313eb85e33cd3
Delfina Buil <delfinabuil2@gmail.com>
2024-06-24 11:28:28 -0300

    Add class diagram for TextEditor

--- src/main/java/texteditor/command/DeleteWordCommand.java
+++ src/main/java/texteditor/command/DeleteWordCommand.java
@@ -3,11 +3,6 @@ package texteditor.command;
 import texteditor.TextEditor;
 
 public class DeleteWordCommand implements Command {
-    // write fields to store state
-    // write a constructor to initialize the fields
-    // implement the execute method
-    // implement the undo method
-
     private int linePos;
     private int pos;
     private String word;
